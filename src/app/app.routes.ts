5032f25dcb78a7e056db016635a41f4fd74638a2
Felipe Torres <felipet73@hotmail.com>
2025-09-08 00:02:03 -0500

    Agregando rutas, menu, probado fin tarea 5

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { UsuariosComponent } from './Administracion/usuarios/usuarios.component'
 import { RolesComponent } from './Administracion/roles/roles.component';
 import { AccesosComponent } from './Administracion/accesos/accesos.component';
 import { NuevoClienteComponent } from './cliente/nuevo-cliente/nuevo-cliente.component';
+import { ProductoComponent } from './producto/producto.component';
+import { NuevoProductoComponent } from './producto/nuevo-producto/nuevo-producto.component';
 
 export const routes: Routes = [
   {
@@ -19,6 +21,21 @@ export const routes: Routes = [
   {
     path: 'editar-cliente/:parametro',
     component: NuevoClienteComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'productos',
+    component: ProductoComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'nuevo-producto',
+    component: NuevoProductoComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'editar-producto/:parametro',
+    component: NuevoProductoComponent,
     pathMatch: 'full',
   },
   {
